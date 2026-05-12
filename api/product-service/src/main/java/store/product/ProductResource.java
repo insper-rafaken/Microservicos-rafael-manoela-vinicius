ba9d8afe91a8967d110318489c6ae2927027861b
Rafael <rafaelkrm@al.insper.edu.br>
2026-05-12 13:31:25 -0300

    aplica injecao por construtor e transactional no product-service

--- api/product-service/src/main/java/store/product/ProductResource.java
+++ api/product-service/src/main/java/store/product/ProductResource.java
@@ -3,15 +3,17 @@ package store.product;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductResource implements ProductController {
 
-    @Autowired
-    private ProductService service;
+    private final ProductService service;
+
+    public ProductResource(ProductService service) {
+        this.service = service;
+    }
 
     @Override
     public ResponseEntity<ProductOut> create(ProductIn in) {
